524d88d32fccee0cd6a9db24861b904c9bdc34bf
Michael Lin <admin@haer0248.me>
2023-03-08 21:42:29 +0800

    add search box

--- docs/.vuepress/plugin.ts
+++ docs/.vuepress/plugin.ts
@@ -1,6 +1,7 @@
 import type { PluginConfig } from '@vuepress/core'
 import { seoPlugin } from 'vuepress-plugin-seo2'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+const { ALGOLIA_DOCSEARCH_API_KEY, ALGOLIA_DOCSEARCH_APP_ID } = process.env
 
 const plugins: PluginConfig = [
     seoPlugin({
@@ -17,11 +18,59 @@ const plugins: PluginConfig = [
 
             return ogp
         }
-    }),
-
-    // docsearchPlugin({
-    //     // 配置项
-    // }),
+    })
 ]
+
+
+if (ALGOLIA_DOCSEARCH_APP_ID && ALGOLIA_DOCSEARCH_API_KEY) {
+    plugins.push(docsearchPlugin({
+      appId: ALGOLIA_DOCSEARCH_APP_ID,
+      apiKey: ALGOLIA_DOCSEARCH_API_KEY,
+      indexName: 'catpalm-haer0248',
+      placeholder: '搜尋',
+      translations: {
+        button: {
+          buttonText: '搜尋',
+          buttonAriaLabel: '搜尋'
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: '清除查詢',
+            resetButtonAriaLabel: '清除查詢',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消'
+          },
+          startScreen: {
+            recentSearchesTitle: '最近',
+            noRecentSearchesText: '没有最近的搜尋',
+            saveRecentSearchButtonTitle: '保存此搜尋',
+            removeRecentSearchButtonTitle: '從歷史記錄中刪除此搜尋',
+            favoriteSearchesTitle: '最愛',
+            removeFavoriteSearchButtonTitle: '從最愛中刪除此搜尋'
+          },
+          errorScreen: {
+            titleText: '無法取得結果',
+            helpText: '您可能需要檢查您的網路連線。'
+          },
+          footer: {
+            selectText: '選擇',
+            selectKeyAriaLabel: '回車鍵',
+            navigateText: '導航',
+            navigateUpKeyAriaLabel: '向上箭頭',
+            navigateDownKeyAriaLabel: '向下箭頭',
+            closeText: '關閉',
+            closeKeyAriaLabel: '退出鍵',
+            searchByText: 'Search by'
+          },
+          noResultsScreen: {
+            noResultsText: '沒有結果 for',
+            suggestedQueryText: '嘗試搜尋 for',
+            reportMissingResultsText: '此查詢應該返回結果嗎？',
+            reportMissingResultsLinkText: '讓我們知道。'
+          }
+        }
+      }
+    }))
+  }
 
 export default plugins
